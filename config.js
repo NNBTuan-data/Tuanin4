10d7a3a4592869bb53f05c780fd8e8614ac06cee
7OTuaNn <151645214+NNBTuan-data@users.noreply.github.com>
2024-04-06 19:41:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Đéooooo',
     question:'Mê cậu quá àaaaaaa<3333333',
     btnReply: 'Gửi cho Tuấn <3',
-    reply: 'Tuấn ơi đi chơi với tớ không',
+    reply: 'Tuấn ơi đi chơi với tớ nhéeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
     mess: 'Phản hồi của bạn đã được gửi đến Tuấn',
     messDesc: 'Cảm ơn bạn vì đã gửi phản hồi',
     btnAccept: 'Tuấn sẽ phản hồi bạn sớm nhất <3',
