f434299cc68a6e8906b78eb2029b735721fc5a44
7OTuaNn <151645214+NNBTuan-data@users.noreply.github.com>
2024-04-06 19:09:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bạn ấn vào link rồi à!',
+    introDesc: `Thế thì hãy trả lời đi!`,
+    btnIntro: 'Tiếp tục!',
+    title: 'Một nụ hồng dành cho mắt nai 😙',
+    desc: 'Em có yêu tôi không ?????????',
+    btnYes: 'Có <3',
+    btnNo: 'Đéooooo',
+    question:'Mê cậu quá àaaaaaa<3333333',
+    btnReply: 'Gửi cho Tuấn <3',
+    reply: 'Tuấn ơi đi chơi với tớ không',
+    mess: 'Phản hồi của bạn đã được gửi đến Tuấn',
+    messDesc: 'Cảm ơn bạn vì đã gửi phản hồi',
+    btnAccept: 'Tuấn sẽ phản hồi bạn sớm nhất <3',
+    messLink: 'https://www.facebook.com/cua.tuan.94/'
+}
